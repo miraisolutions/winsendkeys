6d47a90bd818cc98b074bd6720d4973e81456a19
Roland Schmid <roland.schmid@mirai-solutions.com>
2025-06-30 14:24:08 +0200

    adapt path to new default

--- scripts/runCodecov.R
+++ scripts/runCodecov.R
@@ -1,3 +1,3 @@
 library(covr)
-sink(paste0(basename(normalizePath(".")), ".Rcheck/covrlog.txt"))
+sink(paste0("check/", basename(normalizePath(".")), ".Rcheck/covrlog.txt"))
 codecov(quiet = FALSE, type = c("tests"), token = "${{ secrets.CODECOV_TOKEN }}")
